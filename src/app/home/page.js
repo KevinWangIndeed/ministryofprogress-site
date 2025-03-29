90bd1ff39f0992ec257d1977a023a113a3c2aaf9
Kevin Wang <kevin_why@outlook.com>
2025-03-29 15:25:33 +1100

    修复导入Link和Button的报错

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,6 +1,8 @@
 'use client';
 
 import { useState } from 'react';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
 
 export default function HomePage() {
   const [messages, setMessages] = useState([
